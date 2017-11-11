a8be0719fbb136f8c4bca9909beae88f80839579
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 19:44:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season|
-    season.values
+    season.each do |holiday|
+      holiday.each do |supply|
+        newArr.push(supply)
+      end
+    end
   end
 end
 

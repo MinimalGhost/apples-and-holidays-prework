c6f23a2eb2280868a3cd5b95e14067738cb9e8ea
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 19:47:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season|
     season.each do |holiday|
-    end
+      newArr.push(holiday.values)
     end
   end
   newArr.join(',')

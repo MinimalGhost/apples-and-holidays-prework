1ca96e2590d57bca8d5cbe101450de3c1b4844da
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 20:16:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    # newArr.push("#{key.to_s.capitalize}:")
+    newArr.push("#{key.to_s.capitalize}:")
     puts value
   end
 end

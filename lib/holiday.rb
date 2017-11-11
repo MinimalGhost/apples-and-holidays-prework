44894421a6db5b93eaf52e9866ae2c7740c34619
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 20:40:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   result = []
   holiday_hash.each do |season, season_value|
-    season_value.each do |holiday, holiday_data|
+    season_value.each do |holiday, supplies|
       if holiday_data.include?('BBQ')
         result << seasons.to_sym
       end

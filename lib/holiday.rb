85c6dc2090300ce97d8c37a59b77a46d069d4f22
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-11 19:57:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  puts "#{holiday_hash[:winter]}:"
+  puts "#{holiday_hash[:winter].to_s}:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
